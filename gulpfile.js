b03fbb537c4bf7ee95adb5d64f521c9f5f839b53
jacky77777 <j100401042@gmail.com>
2020-04-21 10:17:35 +0800

    4/21

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,12 @@ watch = require('gulp-watch');
 fileinclude = require('gulp-file-include');
 
 //搬家
-gulp.task('concatjs', function () {
+gulp.task('concatjs', function() {
     //do sometime
     gulp.src('./dev/html/**/*.js').pipe(gulp.dest('dest/'))
 });
 
-gulp.task('concatcss', function () {
+gulp.task('concatcss', function() {
     //do sometime
     gulp.src('./dev/css/*.css').pipe(gulp.dest('./dest/css'))
 });
@@ -21,13 +21,13 @@ gulp.task('concatcss', function () {
 //編譯scss
 
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     gulp.src('./dev/html/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dest/'))
 });
 //壓縮檔案
-gulp.task('minicss', ['sass'], function () {
+gulp.task('minicss', ['sass'], function() {
     gulp.src('dev/css/*.css')
         //壓縮
         .pipe(cleanCSS({
@@ -41,11 +41,11 @@ gulp.task('minicss', ['sass'], function () {
         .pipe(gulp.dest('dest/mini/css'));
 });
 //完成刪除舊檔名
-gulp.task('clean', function () {
+gulp.task('clean', function() {
     del(['dest/mini/css/style.css']);
 });
 
-gulp.task('fileinclude', function () {
+gulp.task('fileinclude', function() {
     gulp.src(['dev/html/**/*.html'])
         .pipe(fileinclude({
             prefix: '@@',
@@ -61,17 +61,17 @@ gulp.task('fileinclude', function () {
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
-gulp.task('default', function () {
+gulp.task('default', function() {
     browserSync.init({
         server: {
             //根目錄
             baseDir: "./",
-            index: "dest/index/index.html"
-            // index: "dest/btn-style/btn-style.html"
+            index: "dest/forum/forum.html"
+                // index: "dest/btn-style/btn-style.html"
         }
     });
 
-    gulp.watch(["sass/*.scss", "sass/**/*.scss","dev/layout/**/*.scss","dev/html/**/*.scss"], ['sass']).on('change', reload);
+    gulp.watch(["sass/*.scss", "sass/**/*.scss", "dev/layout/**/*.scss", "dev/html/**/*.scss"], ['sass']).on('change', reload);
     gulp.watch(["dev/*.html", "dev/**/*.html"], ['fileinclude']).on('change', reload);
-    gulp.watch(["dev/*.js", "dev/**/*.js","dev/layout/**/*.js"], ['concatjs']).on('change', reload);
+    gulp.watch(["dev/*.js", "dev/**/*.js", "dev/layout/**/*.js"], ['concatjs']).on('change', reload);
 });
